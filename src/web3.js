776770ccc1665ed3d09ba39da428f036959ce12f
Richard Corke <richard.corke@and.digital>
2021-12-01 14:01:54 +0100

    add front end skeleton and web3 module

---
+++ src/web3.js
@@ -0,0 +1,8 @@
+import Web3 from "web3";
+
+// lock in to the metamask web3 provider, which already has direct access to rinkeby test net
+window.ethereum.request({ method: "eth_requestAccounts" });
+
+const web3 = new Web3(window.ethereum);
+
+export default web3;
