692f7c9608ee6c7b8e67732cb194811e7943c550
Richard Corke <richard.corke@and.digital>
2022-02-02 10:30:00 +0100

    add balance and players

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { manager: "" };
-  }
+  state = {
+    manager: "",
+    players: 0,
+    balance: "",
+  };
   async componentDidMount() {
     // don't need to specify a 'from' argument inside call(), as metamask has a default account set.
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call().length();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
   render() {
     return (
       <div>
         <h1>Lottery Contract</h1>
         <p>Contract managed by {this.state.manager}</p>
+        <p>Currently there are {this.state.players} players.</p>
+        <p>
+          There is {web3.utils.fromWei(this.state.balance, "ether")} in the pool
+          right now.
+        </p>
       </div>
     );
   }
